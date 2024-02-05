8ec1182d2f9277484960f0dae1908ea43303ccd1
TimurTaichin <timur.taichin@gmail.com>
2024-02-05 16:36:02 +0500

    сделана задача Числа-цифры

--- app.py
+++ app.py
@@ -5,10 +5,15 @@
 # print("Глаша")
 # print("Маша")
 # print("кот")
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-for i in a:
-    if i < 5:
-        print(i)
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# for i in a:
+#     if i < 5:
+#         print(i)
+x = int(input("Введите число для суммы:"))
+y = int(input("Введите число для суммы:"))
+result = (x + y)
+print(result)
+
 
 
 
