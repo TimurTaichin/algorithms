7bef0909b5d3d90744155cf30da8d253084dedb9
TimurTaichin <timur.taichin@gmail.com>
2024-02-05 16:13:10 +0500

    сделал задачу поиск в списке

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-print("bobr")
-print("j")
-print("Женя")
-print("Сеня")
-print("Глаша")
-print("Маша")
-print("кот")
+# print("bobr")
+# print("j")
+# print("Женя")
+# print("Сеня")
+# print("Глаша")
+# print("Маша")
+# print("кот")
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for i in a:
+    if i < 5:
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
