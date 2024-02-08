3e3c9c7204293be8e69864dd5109724bfe3e7c65
TimurTaichin <timur.taichin@gmail.com>
2024-02-08 15:42:01 +0500

    сделана задача "Тип файла"

--- app.py
+++ app.py
@@ -9,11 +9,29 @@
 # for i in a:
 #     if i < 5:
 #         print(i)
-x = int(input("Введите число для суммы:"))
-y = int(input("Введите число для суммы:"))
-result = (x + y)
-print(result)
+# x = int(input("Введите число для суммы:"))
+# y = int(input("Введите число для суммы:"))
+# result = (x + y)
+# print(result)
+# a = [1, 2, 3, 11, 2, 2, 311, 2, 2, 12, 3, 12, 31213, 2, 123, 12]
+# b = [12, 3, 1, 2, 12, 323, 12, 32, 7, 18273, 26, 12, 73426, 12]
 
+# for i in a:
+#     if i != v:
+#         print(i)
+# for v in b:
+#     if v != i:
+#         break
+files = ["main.py, game.exe, doc.txt"]
+nameOfFile = input("Введите название файла: ")
+if nameOfFile == "main.py":
+    print(".py")
+elif nameOfFile == "doc.txt":
+    print(".txt")
+elif nameOfFile == "game.exe":
+    print(".exe")
+else:
+    print("тип файла не найден")
 
 
 
